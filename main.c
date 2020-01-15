fb95097726d320b3acf8d16c989217909d29870b
Jon Magnus <jmlauvra@stud.ntnu.no>
2020-01-15 16:34:14 +0100

    removed conflict markers

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello world\n")
-<<<<<<< HEAD
+	printf("Hello world\n");
+	printf("...and Jupiter\n");
+	printf("Hallo, daniedsv");
 
 	if(1 < 0){// In C, returning something non-zero signals an error
 		return 1;
 	}
-=======
-	printf("...and Mars\n")
->>>>>>> other
+	
 	return 0;
 }
