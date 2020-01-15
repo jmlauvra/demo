794dab763657fb28bf788baa98a6199102a3b985
Jon Magnus <jmlauvra@stud.ntnu.no>
2020-01-15 16:59:49 +0100

    added branch

--- main.c
+++ main.c
@@ -4,7 +4,8 @@ int main(){
 	printf("Hello world\n");
 	printf("...and Jupiter\n");
 	printf("Hallo, daniedsv");
-    printf("Hei, Jon");
+    	printf("Hei, Jon");
+	printf("Naa skal vi lage en branch");
 
 	if(1 < 0){// In C, returning something non-zero signals an error
 		return 1;
